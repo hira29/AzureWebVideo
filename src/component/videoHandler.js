2d1687b32094672c47fbb64b4817b96608097f51
hira29 <dzakydio@gmail.com>
2021-07-23 17:27:42 +0700

    edit changes

--- src/component/videoHandler.js
+++ src/component/videoHandler.js
@@ -31,10 +31,10 @@ class VideoHandler extends Component {
                         src={[{ src: this.state.link, type: "application/vnd.ms-sstr+xml" }]}
                     />
                 </div>
-                <iframe className="center px-10 py-5" src="https://drive.google.com/file/d/1kQCGYwKtgFJtR2ohb_PGTCybyXAfZiKh/preview" width="640" height="480"></iframe>
+                {/* <iframe className="center px-10 py-5" src="https://drive.google.com/file/d/1kQCGYwKtgFJtR2ohb_PGTCybyXAfZiKh/preview" width="640" height="480"></iframe>
                 <iframe className="center px-10 py-5" src="https://drive.google.com/file/d/1IvivqmR5UEgDzo0TH0wN46CdrsWo06PS/preview" width="640" height="480"></iframe>
                 <iframe className="center px-10 py-5" src="https://drive.google.com/file/d/1t1jgAAyzVhsPvEhIsWarSFabzVO8GCRE/preview" width="640" height="480"></iframe>
-                <iframe className="center px-10 py-5" src="https://drive.google.com/file/d/1DjLNWfjcxxRDxUHHdejbcUFW9bPLSpbo/preview" width="640" height="480"></iframe>
+                <iframe className="center px-10 py-5" src="https://drive.google.com/file/d/1DjLNWfjcxxRDxUHHdejbcUFW9bPLSpbo/preview" width="640" height="480"></iframe> */}
             </div>
          );
     }
